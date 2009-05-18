be21da35815844f49faadf9543ce17e68b4abb5e
tsupo <tsujimura543@gmail.com>
2009-05-19 04:26:54 +0900

    changed distribution-batch for github

--- dist.bat
+++ dist.bat
@@ -1,7 +1,7 @@
 echo off
 
 REM batch file for distribution of "subcrawler"
-REM $Header: /comm/subcrawler/dist.bat 1     09/05/14 3:53 tsupo $
+REM $Header: /comm/subcrawler/dist.bat 2     09/05/19 4:25 tsupo $
 
 chmod -R +w dist
 
@@ -11,10 +11,13 @@ mkdir dist\src
 mkdir dist\src\include
 mkdir dist\src\lib
 
-copy *.c dist\src
+copy *.c            dist\src
+copy subcrawler.dsw dist\src
+copy subcrawler.dsp dist\src
 
 copy Release\subcrawler.exe dist
-copy Release\xmlRPC.dll dist
+copy Release\xmlRPC.dll     dist
+copy README                 dist
 
 copy ..\xmlRPC\xmlRPC.h dist\src\include
 copy ..\xmlRPC\Release\xmlRPC.lib dist\src\lib
